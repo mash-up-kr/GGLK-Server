c62ea87af2a0b4fd280662659e323fbd5d7b913b
J-Hoplin <hoplin.dev@gmail.com>
2025-05-05 14:29:18 +0900

    Style: Fix sort import

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
+import { NestFactory } from '@nestjs/core';
 import {
   ApplicationNestConfig,
   ApplicationRedocConfig,
   ApplicationSwaggerConfig,
 } from '@/app.config';
 import { AppModule } from '@/app.module';
-import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
