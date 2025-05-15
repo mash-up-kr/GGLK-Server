cab4372f92be37f8249e02fc32fe23dc6bfb83f9
chsjen492 <chsjen492@gmail.com>
2025-05-15 20:53:31 +0900

    feat: Base Entity에 updatedAt 추가

--- src/common/entity/base.entity.ts
+++ src/common/entity/base.entity.ts
@@ -1,8 +1,11 @@
-import { Column, CreateDateColumn } from 'typeorm';
+import { Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class BaseEntity {
   @CreateDateColumn()
   createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 
   @Column({ default: false, select: false })
   isDeleted: boolean;
