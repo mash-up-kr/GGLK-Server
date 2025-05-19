26a7a6738d875c3de523114644f1eb32b7ecd7c9
Joo-Byungho <kjcoco13@gmail.com>
2025-05-19 16:22:11 +0900

    feat: use mock factories for services and repositories in evaluation, picture, and user tests

--- src/evaluation/evaluation.controller.spec.ts
+++ src/evaluation/evaluation.controller.spec.ts
@@ -1,20 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import {
+  MockServiceFactory,
+  MockedClass,
+} from '@gglk/common/testing/mock.factory';
 import { EvaluationController } from './evaluation.controller';
 import { EvaluationService } from './evaluation.service';
 
 describe('EvaluationController', () => {
   let controller: EvaluationController;
+  let mockedService: MockedClass<EvaluationService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EvaluationController],
-      providers: [EvaluationService],
+      providers: [
+        {
+          provide: EvaluationService,
+          useFactory: MockServiceFactory.getMockService(EvaluationService),
+        },
+      ],
     }).compile();
 
     controller = module.get<EvaluationController>(EvaluationController);
+    mockedService =
+      module.get<MockedClass<EvaluationService>>(EvaluationService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(mockedService).toBeDefined();
   });
 });
--- src/evaluation/evaluation.service.spec.ts
+++ src/evaluation/evaluation.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import {
+  MockRepositoryFactory,
+  MockedClass,
+} from '@gglk/common/testing/mock.factory';
+import { EvaluationRepository } from './evaluation.repository';
 import { EvaluationService } from './evaluation.service';
 
 describe('EvaluationService', () => {
   let service: EvaluationService;
+  let mockedRepositry: MockedClass<EvaluationRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EvaluationService],
+      providers: [
+        EvaluationService,
+        {
+          provide: getRepositoryToken(EvaluationRepository),
+          useFactory:
+            MockRepositoryFactory.getMockRepository(EvaluationRepository),
+        },
+      ],
     }).compile();
 
     service = module.get<EvaluationService>(EvaluationService);
+    mockedRepositry = module.get<MockedClass<EvaluationRepository>>(
+      getRepositoryToken(EvaluationRepository),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(mockedRepositry).toBeDefined();
   });
 });
