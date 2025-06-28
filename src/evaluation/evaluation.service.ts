ccfa375a074bfafe41cf48ebedcb8c36251cb4ae
Joo-Byungho <kjcoco13@gmail.com>
2025-06-29 02:29:50 +0900

    feat: converted url come on

--- src/evaluation/evaluation.service.ts
+++ src/evaluation/evaluation.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AiService } from '@gglk/ai/ai.service';
 import { Evaluation } from './entities/evaluation.entity';
 import { EvaluationRepository } from './evaluation.repository';
@@ -8,13 +9,15 @@ export class EvaluationService {
   constructor(
     private readonly evaluationRepository: EvaluationRepository,
     private readonly aiService: AiService,
+    private readonly configService: ConfigService,
   ) {}
 
   async findById(id: number) {
-    return await this.evaluationRepository.findOne({
-      where: { id },
-      relations: { picture: true },
-    });
+    return await this.evaluationRepository.findByIdWithConvertedUrl(
+      id,
+      this.configService.get('OBJECT_STORAGE_PATTERN') ?? '',
+      this.configService.get('CDN_PATTERN') ?? '',
+    );
   }
 
   async createWithRoasting(
