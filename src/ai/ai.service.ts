03393b521d06a811001161165fe4f61547b066cf
Choi-JY1107 <cjy11230@kakao.com>
2025-07-10 22:53:28 +0900

    feat: ClovaStudio POC

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ChatOpenAI } from '@langchain/openai';
+import OpenAI from 'openai';
 import {
   OotdRoastingAnalysisPrompt,
   OotdRoastingAnalysisSchema,
 } from '@gglk/ai/schemas/evaluation.schema';
 import { PictureNotFoundException } from '@gglk/picture/exceptions';
 import { PictureRepository } from '@gglk/picture/picture.repository';
+import { SYSTEM_PROMPT_3 } from './prompt/system-prompt';
 
 @Injectable()
 export class AiService {
   private readonly chatModel: ChatOpenAI;
+  private readonly client: OpenAI;
 
   constructor(
     private readonly configService: ConfigService,
@@ -20,6 +23,11 @@ export class AiService {
       apiKey: this.configService.get('OPENAI_API_KEY'),
       model: 'gpt-4o-mini',
       temperature: 0,
+    });
+
+    this.client = new OpenAI({
+      apiKey: this.configService.get('CLOVASTUDIO_API_KEY'),
+      baseURL: 'https://clovastudio.stream.ntruss.com/v1/openai', // CLOVA Studio OpenAI 호환 API 엔드포인트 입력
     });
   }
 
@@ -51,6 +59,18 @@ export class AiService {
       pictureInstance.url,
       spicyLevel,
     ).formatMessages({});
+
+    console.log('ootdRoastingPrompt:', ootdRoastingPrompt);
+
+    const completion = await this.client.chat.completions.create({
+      model: 'HCX-005',
+      messages: [
+        { role: 'system', content: SYSTEM_PROMPT_3 },
+        { role: 'user', content: pictureInstance.url },
+      ],
+    });
+
+    console.log(completion.choices[0].message.content ?? '');
 
     return await this.chatModel
       .withStructuredOutput(OotdRoastingAnalysisSchema)
